2e25a9819b8c7015392538a8153bece37f4c0b5e
tchamalsha <tharu.chamalsha@gmail.com>
2022-07-01 23:28:14 +0530

    connect bootstrap and router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+import './axios'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+
+createApp(App).use(router).use(router).mount('#app')
---
+++ src/views/DashboardView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+      <h2>Welcome Tharushi!!</h2>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+h2{text-align: left;}
+
+</style>
