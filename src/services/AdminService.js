2a9655b4624211fc86a345386071d7322460a2a8
tchamalsha <tharu.chamalsha@gmail.com>
2022-11-09 12:19:27 +0530

    add user conrtoll

--- src/services/AdminService.js
+++ src/services/AdminService.js
@@ -14,33 +14,7 @@ class AdminService{
         })
     }
 
-    async signup(form) {
-        return axios.post('admin/signup', {
-            id: form.id,
-            name: form.name,
-            email: form.email,
-            password: form.password,
-            position:form.position
-            
-        }),axios.post('signup/personalDetails',{
-            id: form.id,
-            address: form.address,
-            postalCode: form.postalCode,
-            telephone: form.telephone,
-        }),axios.post('signup/salaryDetails', {
-            id:form.id,
-            basicSalary:form.basicSalary,
-            otRate:form.otRate,
-            specialAllowance:form.specialAllowance
-            
-        }).then((res) => {
-            this.$router.push('/success')
-            return res
-        }).catch((err) => {
-            this.$router.push('/error')
-            throw err
-        }) 
-    }
+    
 }
 
 export default new AdminService;
